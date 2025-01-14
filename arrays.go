8cb716284218c78b5dee288163b219f2c40e9f39
NikitaBelikov <belsta.nik@gmail.com>
2025-01-14 12:09:46 +0300

    looping through an array

--- arrays.go
+++ arrays.go
@@ -13,12 +13,27 @@ import "fmt"
 // 	message := fmt.Sprintln(arr[1], arr[0], arr[3], arr[2])
 // 	fmt.Print(message) // Вывод: Get ready to Go
 // }
+// ================================
+// func message() string {
+// 	arr := [4]string{"ready", "Get", "Go", "to"}
+// 	arr[1] = "It's"
+// 	arr[0] = "time"
+// 	return fmt.Sprintln(arr[1], arr[0], arr[3], arr[2])
+// }
 
+// func main() {
+// 	fmt.Print(message())
+// }
+// ===============================
 func message() string {
-	arr := [4]string{"ready", "Get", "Go", "to"}
-	arr[1] = "It's"
-	arr[0] = "time"
-	return fmt.Sprintln(arr[1], arr[0], arr[3], arr[2])
+	m := ""
+	arr := [4]int{1,2,3,4}
+
+	for i := 0; i < len(arr); i++ {
+		arr[i] = arr[i] * arr[i]
+		m += fmt.Sprintf("%v: %v\n", i, arr[i])
+	}
+	return m
 }
 
 func main() {
