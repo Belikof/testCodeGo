2c85ad5907bcc4a7fcae4648c9534e635c9393f8
NikitaBelikov <belsta.nik@gmail.com>
2025-01-14 12:17:03 +0300

    making changes to the array

--- arrays.go
+++ arrays.go
@@ -25,17 +25,38 @@ import "fmt"
 // 	fmt.Print(message())
 // }
 // ===============================
-func message() string {
-	m := ""
-	arr := [4]int{1,2,3,4}
+// func message() string {
+// 	m := ""
+// 	arr := [4]int{1,2,3,4}
 
+// 	for i := 0; i < len(arr); i++ {
+// 		arr[i] = arr[i] * arr[i]
+// 		m += fmt.Sprintf("%v: %v\n", i, arr[i])
+// 	}
+// 	return m
+// }
+
+// func main() {
+// 	fmt.Print(message())
+// }
+// =============================
+func fillArray(arr [10]int) [10]int {
+	for i := 0; i < len(arr); i++ {
+		arr[i] = i + 1
+	}
+	return arr
+}
+
+func opArray(arr [10]int) [10]int {
 	for i := 0; i < len(arr); i++ {
 		arr[i] = arr[i] * arr[i]
-		m += fmt.Sprintf("%v: %v\n", i, arr[i])
 	}
-	return m
+	return arr
 }
 
 func main() {
-	fmt.Print(message())
+	var arr [10]int
+	arr = fillArray(arr)
+	arr = opArray(arr)
+	fmt.Println(arr)
 }
