3f5630444f59f644e6f25f980c2b6f7424ff667b
NikitaBelikov <belsta.nik@gmail.com>
2025-01-14 12:01:20 +0300

    Array modification function

--- arrays.go
+++ arrays.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	arr := [...]string{
+// 		"ready",
+// 		"Get",
+// 		"Go",
+// 		"to",
+// 	}
+// 	// Формируем сообщение из массива
+// 	message := fmt.Sprintln(arr[1], arr[0], arr[3], arr[2])
+// 	fmt.Print(message) // Вывод: Get ready to Go
+// }
+
+func message() string {
+	arr := [4]string{"ready", "Get", "Go", "to"}
+	arr[1] = "It's"
+	arr[0] = "time"
+	return fmt.Sprintln(arr[1], arr[0], arr[3], arr[2])
+}
+
 func main() {
-	arr := [...]string{
-		"ready",
-		"Get",
-		"Go",
-		"to",
-	}
-	// Формируем сообщение из массива
-	message := fmt.Sprintln(arr[1], arr[0], arr[3], arr[2])
-	fmt.Print(message) // Вывод: Get ready to Go
+	fmt.Print(message())
 }
