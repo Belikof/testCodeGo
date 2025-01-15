b814bb15325ce4cc78b408f11b48fa3b6c6854b9
NikitaBelikov <belsta.nik@gmail.com>
2025-01-15 13:10:55 +0300

    Using make for slice capacity control

--- slice.go
+++ slice.go
@@ -55,16 +55,30 @@ import ("fmt"
 // 	fmt.Println("Locales to use:", locales)
 // }
 
-func message() string {
-	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	m := fmt.Sprintln("First:", s[0], s[0:1], s[:1])
-	m += fmt.Sprintln("Last:", s[len(s)-1], s[len(s)-1:len(s)], s[len(s)-1:])
-	m += fmt.Sprintln("First 5:", s[:5])
-	m += fmt.Sprintln("Last 4 :", s[5:])
-	m += fmt.Sprintln("Middle 5:", s[2:7])
-	return m
+// func message() string {
+// 	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+// 	m := fmt.Sprintln("First:", s[0], s[0:1], s[:1])
+// 	m += fmt.Sprintln("Last:", s[len(s)-1], s[len(s)-1:len(s)], s[len(s)-1:])
+// 	m += fmt.Sprintln("First 5:", s[:5])
+// 	m += fmt.Sprintln("Last 4 :", s[5:])
+// 	m += fmt.Sprintln("Middle 5:", s[2:7])
+// 	return m
+// }
+
+// func main() {
+// 	fmt.Print(message())
+// }
+
+func genSlices() ([]int, []int, []int) {
+	var s1 []int
+	s2 := make([]int, 10)
+	s3 := make([]int, 10, 50)
+	return s1, s2, s3
 }
 
 func main() {
-	fmt.Print(message())
+	s1, s2, s3 := genSlices()
+	fmt.Printf("s1: len = %v cap = %v\n", len(s1), cap(s1))
+	fmt.Printf("s2: len = %v cap = %v\n", len(s2), cap(s2))
+	fmt.Printf("s3: len = %v cap = %v\n", len(s3), cap(s3))
 }
