2441c16b9d5d76d36ab1e873bb0960571833a298
NikitaBelikov <belsta.nik@gmail.com>
2025-01-15 12:56:15 +0300

    Creating slices from a slice

--- slice.go
+++ slice.go
@@ -1,7 +1,7 @@
 package main
 
 import ("fmt"
-		"os"
+
 )
 
 // func getPassedArgs(minArgs int) []string {
@@ -35,22 +35,36 @@ import ("fmt"
 // 	}
 // }
 
-func getPassedArgs() []string {
-	var args []string
-	for i := 1; i < len(os.Args); i++ {
-		args = append(args, os.Args[i])
-	}
-	return args
-}
+// func getPassedArgs() []string {
+// 	var args []string
+// 	for i := 1; i < len(os.Args); i++ {
+// 		args = append(args, os.Args[i])
+// 	}
+// 	return args
+// }
 
-func getLocals(extraLocals []string) []string {
-	var locales []string
-	locales = append(locales, "en_US", "fr_FR")
-	locales = append(locales, extraLocals...)
-	return locales
+// func getLocals(extraLocals []string) []string {
+// 	var locales []string
+// 	locales = append(locales, "en_US", "fr_FR")
+// 	locales = append(locales, extraLocals...)
+// 	return locales
+// }
+
+// func main() {
+// 	locales := getLocals(getPassedArgs())
+// 	fmt.Println("Locales to use:", locales)
+// }
+
+func message() string {
+	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	m := fmt.Sprintln("First:", s[0], s[0:1], s[:1])
+	m += fmt.Sprintln("Last:", s[len(s)-1], s[len(s)-1:len(s)], s[len(s)-1:])
+	m += fmt.Sprintln("First 5:", s[:5])
+	m += fmt.Sprintln("Last 4 :", s[5:])
+	m += fmt.Sprintln("Middle 5:", s[2:7])
+	return m
 }
 
 func main() {
-	locales := getLocals(getPassedArgs())
-	fmt.Println("Locales to use:", locales)
+	fmt.Print(message())
 }
