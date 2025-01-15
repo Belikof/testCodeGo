b8aed22adac918fffb8f1193aa1023da5952b235
NikitaBelikov <belsta.nik@gmail.com>
2025-01-15 13:42:42 +0300

    Behavior management internal cut

--- slice.go
+++ slice.go
@@ -69,16 +69,72 @@ import ("fmt"
 // 	fmt.Print(message())
 // }
 
-func genSlices() ([]int, []int, []int) {
-	var s1 []int
-	s2 := make([]int, 10)
-	s3 := make([]int, 10, 50)
-	return s1, s2, s3
+// func genSlices() ([]int, []int, []int) {
+// 	var s1 []int
+// 	s2 := make([]int, 10)
+// 	s3 := make([]int, 10, 50)
+// 	return s1, s2, s3
+// }
+
+// func main() {
+// 	s1, s2, s3 := genSlices()
+// 	fmt.Printf("s1: len = %v cap = %v\n", len(s1), cap(s1))
+// 	fmt.Printf("s2: len = %v cap = %v\n", len(s2), cap(s2))
+// 	fmt.Printf("s3: len = %v cap = %v\n", len(s3), cap(s3))
+// }
+
+func linked() (int, int, int) {
+	s1 :=[]int{1, 2, 3, 4, 5}
+	s2 := s1
+	s3 := s1[:]
+	s1[3] = 99
+	return s1[3], s2[3], s3[3]
+}
+
+func noLink() (int, int) {
+	s1 := []int{1, 2, 3, 4, 5}
+	s2 := s1
+	s1 = append(s1, 6)
+	s1[3] = 99
+	return s1[3], s2[3]
+}
+
+func capLinked() (int, int) {
+	s1 := make([]int, 5, 10)
+	s1[0], s1[1], s1[2], s1[3], s1[4] = 1, 2, 3, 4, 5
+	s2 := s1
+	s1 = append(s1, 6)
+	s1[3] = 99
+	return s1[3], s2[3]
+}
+
+func copyNoLink() (int, int, int) {
+	s1 := []int{1, 2, 3, 4, 5}
+	s2 := make([]int, len(s1))
+	copied := copy(s2, s1)
+	s1[3] = 99
+	return s1[3], s2[3], copied
+}
+
+func appendNoLink() (int, int) {
+	s1 := []int{1, 2, 3, 4, 5}
+	s2 := append([]int{}, s1...)
+	s1[3] = 99
+	return s1[3], s2[3]
 }
 
 func main() {
-	s1, s2, s3 := genSlices()
-	fmt.Printf("s1: len = %v cap = %v\n", len(s1), cap(s1))
-	fmt.Printf("s2: len = %v cap = %v\n", len(s2), cap(s2))
-	fmt.Printf("s3: len = %v cap = %v\n", len(s3), cap(s3))
+	l1, l2, l3 := linked()
+	fmt.Println("Linked :", l1, l2, l3)
+	nl1, nl2 := noLink()
+	fmt.Println("No Link :", nl1, nl2)
+	cl1, cl2 := capLinked()
+	fmt.Println("Cap Link :", cl1, cl2)
+	cnl1, cnl2 := capNoLink()
+	fmt.Println("Cap No Link :", cnl1, cnl2)
+	copy1, copy2, copied := copyNoLink()
+	fmt.Print("Copy No Link: ", copy1, copy2)
+	fmt.Printf(" (Number of elements copied %v)\n", copied)
+	a1, a2 := appendNoLink()
+	fmt.Println("Append No Link:", a1, a2)
 }
