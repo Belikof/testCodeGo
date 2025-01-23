790344ea74a0c0c682257c418147202d5b56b20b
NikitaBelikov <belsta.nik@gmail.com>
2025-01-23 12:51:34 +0300

    adding validation to the constructor structure

--- validation.go
+++ validation.go
@@ -3,47 +3,87 @@ package main
 import (
 	"fmt"
 	"errors"
+	"time"
 	"strings"
 )
 
 
-type User struct {
-	name string
-	age int
-	email string
+// type User struct {
+// 	name string
+// 	age int
+// 	email string
+// }
+
+// func newUser (name string, age int, email string) (*User, error) {
+// 	if name == "" {
+// 		return nil, errors.New("имя не может быть пустым")
+// 	} else if len(name) < 3 {
+// 		return nil, errors.New("длина должна быть не менее 3 символов")
+// 	}
+// 	if age < 18 || age > 100 {
+// 		return nil, errors.New("не подходящий возраст")
+// 	}
+// 	if !strings.Contains(email, "@") || !strings.Contains(email, ".") {
+// 		return nil, errors.New("некорректный email: должен содержать '@' и '.'")
+// 	}
+	
+// 	return &User {
+// 		name: name,
+// 		age: age,
+// 		email: email,
+// 	}, nil
+// }
+
+// func printUserInfo(user *User) {
+// 	fmt.Println("Имя", user.name)
+// 	fmt.Println("Возраст", user.age)
+// 	fmt.Println("Почта", user.email)
+// }
+
+// func main() {
+// 	user, err := newUser("Alice", 25, "alice@example.com")
+// 	if err != nil {
+// 		fmt.Println("Ошибка:", err)
+// 		return
+// 	}
+// 	printUserInfo(user)
+// }
+
+type Car struct {
+	brand string
+	year int
+	licencePlate string
 }
 
-func newUser (name string, age int, email string) (*User, error) {
-	if name == "" {
-		return nil, errors.New("имя не может быть пустым")
-	} else if len(name) < 3 {
-		return nil, errors.New("длина должна быть не менее 3 символов")
+func newCar (brand string, year int, licencePlate string) (*Car, error) {
+	if brand == "" || len(brand) < 2 {
+		return nil, errors.New("марка  машинв не может быть пустой или содержать меньше 2 символов")
 	}
-	if age < 18 || age > 100 {
-		return nil, errors.New("не подходящий возраст")
+	if year < 1886 || year > time.Now().Year() {
+		return nil, errors.New("не подходящий год выпуска")
 	}
-	if !strings.Contains(email, "@") || !strings.Contains(email, ".") {
-		return nil, errors.New("некорректный email: должен содержать '@' и '.'")
+	if len(licencePlate) < 6 || !strings.ContainsAny(licencePlate, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") || !strings.ContainsAny(licencePlate, "1234567890") {
+		return nil, errors.New("неправильный формат лицензии: должен содержать буквы и цифры")
 	}
-	
-	return &User {
-		name: name,
-		age: age,
-		email: email,
+
+	return &Car{
+		brand: brand, 
+		year: year,
+		licencePlate: licencePlate,
 	}, nil
 }
 
-func printUserInfo(user *User) {
-	fmt.Println("Имя", user.name)
-	fmt.Println("Возраст", user.age)
-	fmt.Println("Почта", user.email)
+func printCarInfo(car *Car) {
+	fmt.Println("Марка", car.brand)
+	fmt.Println("Год", car.year)
+	fmt.Println("Лицензия", car.licencePlate)
 }
 
 func main() {
-	user, err := newUser("Alice", 25, "alice@example.com")
+	car, err := newCar("toyota", 1900, "asdasfasf")
 	if err != nil {
-		fmt.Println("Ошибка:", err)
-		return
+		fmt.Println("Ошибка", err)
+		return 
 	}
-	printUserInfo(user)
+	printCarInfo(car)
 }
