4fd8d162c354da0fc4dda4117d0f47495a00355e
NikitaBelikov <belsta.nik@gmail.com>
2025-01-28 14:11:40 +0300

    десериализация

--- json.go
+++ json.go
@@ -12,17 +12,30 @@ type User struct {
 }
 
 func main() {
-	user := User {
-		Name:  "Alice",
-		Age:   30,
-		Email: "alice@example.com",
-	}
+	// user := User {
+	// 	Name:  "Alice",
+	// 	Age:   30,
+	// 	Email: "alice@example.com",
+	// }
 
-	jsonData, err := json.Marshal(user)
+	// jsonData, err := json.Marshal(user)
+	// if err != nil {
+	// 	fmt.Println("Ошибка кодирования JSON:", err)
+	// 	return
+	// }
+
+	// fmt.Println(string(jsonData))
+	jsonString := `{"name":"Alice","age":30,"email":"alice@example.com"}`
+
+	var user User
+
+	err := json.Unmarshal([]byte(jsonString), &user)
 	if err != nil {
-		fmt.Println("Ошибка кодирования JSON:", err)
+		fmt.Println("Ошибка декодирования JSON", err)
 		return
 	}
 
-	fmt.Println(string(jsonData))
+	fmt.Println("Имя:", user.Name)
+	fmt.Println("Возраст:", user.Age)
+	fmt.Println("Email:", user.Email)
 }
